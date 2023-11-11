0b465312ef2204d52116518e010d1a92afef8111
Diego Parra <dp@linkfire.com>
2023-11-11 16:35:39 -0400

    updates

--- labs/lab1/target/debug/deps/lab1-b84b5ec03d4e463e.d
+++
@@ -1,5 +0,0 @@
-/home/parra/Documents/wkt/personal/rust-setup/labs/lab1/target/debug/deps/lab1-b84b5ec03d4e463e.rmeta: src/main.rs
-
-/home/parra/Documents/wkt/personal/rust-setup/labs/lab1/target/debug/deps/lab1-b84b5ec03d4e463e.d: src/main.rs
-
-src/main.rs:
--- labs/lab1/target/debug/lab1.d
+++
@@ -1,1 +0,0 @@
-/home/parra/Documents/wkt/personal/rust-setup/labs/lab1/target/debug/lab1: /home/parra/Documents/wkt/personal/rust-setup/labs/lab1/src/main.rs
